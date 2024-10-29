bfe2693339fcb0d5f7e97e821cb03a0b44a8933b
alfonsocifpcuenca <alfonso.rodriguez@cifpcuenca.es>
2024-10-29 21:05:53 +0100

    Paddle limits added and comments added

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Ball : MonoBehaviour
 {
-
-    public float launchSpeed = 10f;
+    [SerializeField]
+    private float launchSpeed = 10f;
     private bool isLaunched = false;
     private Rigidbody2D rigidbody2D;
-    public Transform paddle;
 
+    [SerializeField]
+    private Transform paddle;
+
+    // Posición de la bola respecto de la pala
     private Vector2 initialPosition = new Vector2(0.35f, 1.25f);
 
     private void Awake()
     {
+        // Obtenemos el componente Rigidbody2D
         this.rigidbody2D = GetComponent<Rigidbody2D>();
-        if (this.rigidbody2D == null)
-        {
-            Debug.LogError("Rigidbody no encontrado en el objeto Ball. Asegúrate de que el componente Rigidbody esté adjunto.");
-            enabled = false;
-        }
     }
 
     private void Start()
@@ -32,8 +32,10 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Comprobamos si el objeto con el que hemos colisionado (collision.transform) tiene IDamagable
+        // Obtenemos el componente IDamagable del objeto con el que hemos colisionado
         var objectDamagable = collision.gameObject.GetComponent<IDamagable>();
+
+        // Si el objeto es distinto de null, llamamos a su método TakeDamage()
         if (objectDamagable != null)
         {
             objectDamagable.TakeDamage();
@@ -47,16 +49,29 @@ public class Ball : MonoBehaviour
 
     private void LaunchBall()
     {
+        // Si pulsamos la tecla Space y la pelota no ha sido lanzada la lanzamos
         if (Input.GetKeyDown(KeyCode.Space) && !this.isLaunched)
         {
+            // Establecemos la pelota como lanzada
             this.isLaunched = true;
+
+            // Sacamos a la pelota de la pala
             this.transform.parent = null;
+
+            // Establecemos una dirección aleatoria de lanzamiento
             float randomDirection = Random.Range(-1f, 1f);
+
+            // Calculamos la dirección de lanzamiento de la bola
             Vector3 launchDirection = new Vector3(randomDirection, 1, 0).normalized;
+
+            // Aplicamos una velocidad a la bola en la dirección calculada
             this.rigidbody2D.velocity = launchDirection * launchSpeed;
         }
     }
 
+    /// <summary>
+    /// Establece la posición y estados iniciales de la pelota
+    /// </summary>
     private void ResetBallPosition()
     {
         this.isLaunched = false;
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,7 +5,51 @@ public class Paddle : MonoBehaviour
     [SerializeField]
     private float speed = 8f;
 
-    private float limits = 3.15f;
+    [SerializeField]
+    private GameObject leftWall;
+
+    [SerializeField]
+    private GameObject rightWall;
+
+    private float paddleLeftLimit;
+    private float paddleRightLimit;
+
+    private void Start()
+    {
+        this.CalculatePaddleLimits();
+    }
+
+    private void CalculatePaddleLimits()
+    {
+        // Obtenemos el componente SpriteRenderer de la pared izquierda
+        SpriteRenderer leftWallRenderer = this.leftWall.GetComponent<SpriteRenderer>();
+
+        // Obtenemos el componente SpriteRenderer de la pared derecha
+        SpriteRenderer rightWallRenderer = this.rightWall.GetComponent<SpriteRenderer>();
+
+        // Obtenemos el SpriteRenderer de la pala
+        SpriteRenderer paddleRenderer = this.GetComponent<SpriteRenderer>();
+
+        // Si los SpriteRenderers son distintos de null, calculamos los límites
+        if (leftWallRenderer != null && rightWallRenderer != null && paddleRenderer != null)
+        {
+            // Obtenemos el ancho de la pared izquierda
+            float leftWallWidth = leftWallRenderer.bounds.size.x;
+            // Obtenemos el ancho de la pared derecha
+            float rightWallWidth = rightWallRenderer.bounds.size.x;
+            // Obtenemos el ancho de la pala
+            float paddleWidth = paddleRenderer.bounds.size.x;
+
+            // Calculamos el límite izquierdo
+            this.paddleLeftLimit = this.leftWall.transform.position.x + leftWallWidth / 2 + paddleWidth / 2;
+            // Calculamos el límite dereche
+            this.paddleRightLimit = this.rightWall.transform.position.x - rightWallWidth / 2 - paddleWidth / 2;
+        }
+        else
+        {
+            Debug.LogError("One or more required SpriteRenderers are missing.");
+        }
+    }
 
     void Update()
     {
@@ -14,13 +58,22 @@ public class Paddle : MonoBehaviour
     
     private void MovePaddle()
     {
+        // Obtenemos la direccion de movimiento
+        // -1 = izquieda
+        // 0 = no se mueve
+        // 1 = derecha
         float moveInput = Input.GetAxisRaw("Horizontal");
 
+        // Obtenemos la posición actual de la pala en el eje X
         float currentXPosition = this.transform.position.x;
-        float xIncrement = moveInput * this.speed * Time.deltaTime;
+        
+        // Calculamos la nueva posicion de la pala
+        float newXPosition = moveInput * this.speed * Time.deltaTime + currentXPosition;
 
-        float newXPosition = Mathf.Clamp(currentXPosition + xIncrement, -limits, limits);
+        // Acotamos el valor para no exceder los límites
+        newXPosition = Mathf.Clamp(currentXPosition, this.paddleLeftLimit, this.paddleRightLimit);
 
+        // Establecemos la nueva posición a la pala
         Vector3 newPosition = new Vector3(newXPosition, this.transform.position.y, this.transform.position.z);
         this.transform.position = newPosition;
     }
